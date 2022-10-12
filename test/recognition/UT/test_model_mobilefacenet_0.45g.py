a9a194423fa2c1c5437bd1c129bef5902306488a
unknown <shi_haoxiang@163.com>
2022-10-12 18:15:13 +0800

    detection部分UTST更新

--- test/recognition/UT/test_model_mobilefacenet_0.45g.py
+++ test/recognition/UT/test_model_mobilefacenet_0.45g.py
@@ -7,23 +7,17 @@ import mindspore as ms
 from mindspore.parallel import _cost_model_context as cost_model_context
 from mindspore import context, Tensor
 from mindface.recognition.models.mobilefacenet import get_mbf
-import pytest
 
+# iresnet
+__all__ = ['iresnet50', 'iresnet100']
 
-
-def test_model(mode, target):
-    if mode == 'PYNATIVE_MODE':
-        context.set_context(mode=context.PYNATIVE_MODE,device_target='GPU', save_graphs=False)
-    else:
-        context.set_context(mode=context.GRAPH,device_target='GPU', save_graphs=False)
-    if target == 'GPU':
-        context.set_context(mode=context.PYNATIVE_MODE,device_target='GPU', save_graphs=False)
-    else:
-        context.set_context(mode=context.GRAPH,device_target='Ascend', save_graphs=False)
-    bs = 256
-    net = get_mbf(False,512)
-    x = ms.Tensor(np.ones([bs, 3, 112, 112]), ms.float32)
+context.set_context(mode=context.PYNATIVE_MODE,
+                    device_target='GPU', save_graphs=False)
+def test_model():
+    
+    net = get_mbf()
+    x = ms.Tensor(np.ones([4, 3, 112, 112]), ms.float32)
+    print(x.shape)
     output = net(x)
-    assert output.shape[0] == bs, 'output shape not match'
-    print('unit test OK')
-test_model(mode='PYNATIVE_MODE', target='GPU')
+    print(output.shape)
+test_model()
--- test/recognition/UT/test_model_resnet50.py
+++ test/recognition/UT/test_model_resnet50.py
@@ -4,25 +4,22 @@ sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(),'mindface/recognition'))
 import mindspore.numpy as np
 import mindspore as ms
+
 from mindspore.parallel import _cost_model_context as cost_model_context
 from mindspore import context, Tensor
-from mindface.recognition.models.iresnet import iresnet50
-import pytest
 
+from mindface.recognition.models.iresnet import iresnet50, iresnet100
 
-def test_model(mode, target):
-    if mode == 'PYNATIVE_MODE':
-        context.set_context(mode=context.PYNATIVE_MODE,device_target='GPU', save_graphs=False)
-    else:
-        context.set_context(mode=context.GRAPH,device_target='GPU', save_graphs=False)
-    if target == 'GPU':
-        context.set_context(mode=context.PYNATIVE_MODE,device_target='GPU', save_graphs=False)
-    else:
-        context.set_context(mode=context.GRAPH,device_target='Ascend', save_graphs=False)
-    bs = 256
+# iresnet
+__all__ = ['iresnet50', 'iresnet100']
+
+context.set_context(mode=context.PYNATIVE_MODE,
+                    device_target='GPU', save_graphs=False)
+def test_model():
     net = iresnet50()
-    x = ms.Tensor(np.ones([bs, 3, 112, 112]), ms.float32)
+    x = ms.Tensor(np.ones([4, 3, 112, 112]), ms.float32)
+    print(x.shape)
     output = net(x)
-    assert output.shape[0] == bs, 'output shape not match'
-    print('unit test OK')
-test_model(mode='PYNATIVE_MODE', target='GPU')
+    print(output.shape)
+    
+test_model()
