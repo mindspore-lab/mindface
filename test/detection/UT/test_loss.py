cd604dec4d9b74aebf73c787a8501233edb8c6b5
Fly <9904498+czp21@user.noreply.gitee.com>
2022-12-06 19:03:28 +0800

    detection

--- test/detection/UT/test_loss.py
+++ test/detection/UT/test_loss.py
@@ -8,4 +8,4 @@ def test_multiboxloss():
     negative_ratio = 7
     num_classes = 2
     num_anchor = 16800
-    multibox_loss = MultiBoxLoss(num_classes, num_anchor, negative_ratio, batch_size)
+    multibox_loss = MultiBoxLoss(num_classes, num_anchor, negative_ratio)
