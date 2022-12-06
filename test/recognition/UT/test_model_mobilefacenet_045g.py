200f84e89fc622571b229d8e93c386683be31cff
Fly <9904498+czp21@user.noreply.gitee.com>
2022-12-07 00:19:03 +0800

    detection

--- test/recognition/UT/test_model_mobilefacenet_045g.py
+++ test/recognition/UT/test_model_mobilefacenet_045g.py
@@ -12,17 +12,13 @@ import pytest
 
 
 @pytest.mark.parametrize("mode",['PYNATIVE_MODE','GRAPH_MODE'])
-@pytest.mark.parametrize("target",['GPU'])
+# @pytest.mark.parametrize("target",['CPU'])
 
-def test_model(mode, target):
+def test_model(mode):
     if mode == 'PYNATIVE_MODE':
-        context.set_context(mode=context.PYNATIVE_MODE,device_target='GPU', save_graphs=False)
+        context.set_context(mode=context.PYNATIVE_MODE,device_target='CPU', save_graphs=False)
     else:
-        context.set_context(mode=context.GRAPH_MODE,device_target='GPU', save_graphs=False)
-    if target == 'GPU':
-        context.set_context(mode=context.PYNATIVE_MODE,device_target='GPU', save_graphs=False)
-    else:
-        context.set_context(mode=context.GRAPH_MODE,device_target='Ascend', save_graphs=False)
+        context.set_context(mode=context.GRAPH_MODE,device_target='CPU', save_graphs=False)
     bs = 256
     net = get_mbf(False,512)
     x = ms.Tensor(np.ones([bs, 3, 112, 112]), ms.float32)
